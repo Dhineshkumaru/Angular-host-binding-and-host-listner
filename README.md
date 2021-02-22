fa08c8f2aa21fc240864ccb30c74aa6e66d21671
Dhineshkumaru <dhineshkumarugendran@gmail.com>
2021-02-22 08:21:10 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-host-binding-and-host-listner
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-host-binding-and-host-listner)
---
+++ src/app/hello.component.ts
@@ -0,0 +1,22 @@
+import { Component, HostBinding, HostListener, Input } from "@angular/core";
+
+@Component({
+  selector: "hello",
+  template: `
+    <h1>Hello {{ name }}!</h1>
+  `,
+  styles: [
+    `
+      h1 {
+        font-family: Lato;
+      }
+    `
+  ]
+})
+export class HelloComponent {
+  @Input() name: string;
+  @HostBinding("style.color") color = "red";
+  @HostListener("click") function() {
+    this.color = "blue";
+  }
+}
